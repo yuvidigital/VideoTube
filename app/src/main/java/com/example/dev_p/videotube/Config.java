f1767001196634fafc6f5971b5f5641d73091d0e
Devendra Waghmare <Devendra Waghmare>
2017-04-13 00:37:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dev_p/videotube/Config.java
@@ -0,0 +1,16 @@
+package com.example.dev_p.videotube;
+
+/**
+ * Created by dev_p on 4/4/2017.
+ */
+
+public final class Config {
+
+    private Config(){
+        }
+
+    public static final  String YOUTUBE_API_KEY = "AIzaSyAKJ42nSQzVoKYdDOvFmViHBBUFzvnQn30";
+
+
+
+}
